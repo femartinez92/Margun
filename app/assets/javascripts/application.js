bb07f3c474751679a62ee43c1880ebbef71b89de
Fernando Martinez <femartinez@uc.cl>
2014-06-07 12:28:59 -0400

    Primer commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,113 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+//Slider animation -------------------------
+$(document).ready(function() {
+
+	//rotation speed and timer
+	var speed = 5000;
+	var run = setInterval('rotate()', speed);	
+	
+	//grab the width and calculate left value
+	var item_width = $('#slides li').outerWidth(); 
+	var left_value = item_width * (-1); 
+        
+    //move the last item before first item, just in case user click prev button
+	$('#slides li:first').before($('#slides li:last'));
+	
+	//set the default item to the correct position 
+	$('#slides ul').css({'left' : left_value});
+	//if user clicked on prev button
+	$('#prev').click(function() {
+
+		//get the right position            
+		var left_indent = parseInt($('#slides ul').css('left')) + item_width;
+
+		//slide the item            
+		$('#slides ul').animate({'left' : left_indent}, 1000,function(){    
+
+            //move the last item and put it as first item            	
+			$('#slides li:first').before($('#slides li:last'));           
+
+			//set the default item to correct position
+			$('#slides ul').css({'left' : left_value});
+		
+		});
+
+		//cancel the link behavior            
+		return false;
+            
+	});
+
+ 
+    //if user clicked on next button
+	$('#next').click(function() {
+		
+		//get the right position
+		var left_indent = parseInt($('#slides ul').css('left')) - item_width;
+		
+		//slide the item
+		$('#slides ul').animate({'left' : left_indent}, 1200, function () {
+            
+            //move the first item and put it as last item
+			$('#slides li:last').after($('#slides li:first'));                 	
+			
+			//set the default item to correct position
+			$('#slides ul').css({'left' : left_value});
+		
+		});
+		         
+		//cancel the link behavior
+		return false;
+		
+	});        
+	
+
+//if mouse hover, pause the auto rotation, otherwise rotate it
+	$('#slides').hover(
+		
+		function() {
+			clearInterval(run);
+		}, 
+		function() {
+			run = setInterval('rotate()', speed);	
+		}
+	); 
+
+	$('.contacto_minimizado').hover(
+		function(){
+		$('.contacto_maximizado').css('display', 'block');
+		},function(){
+		$('.contacto_maximizado').css('display', 'none');
+		}
+	);
+	$('.contacto_maximizado').hover(
+		function(){
+		$('.contacto_maximizado').css('display', 'block');
+		},function(){
+		$('.contacto_maximizado').css('display', 'none');
+		}
+	);
+
+        
+});
+
+function rotate() {
+	$('#next').click();
+}
+//Termino slider ------------------------------------------------------------------------
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LavinVillegas::Application.config.secret_key_base = '7a186874639539aea245e65ac9d5969f691957495495a2d3fb35c8950cc7d7e7e43e03e7231e0c64756674b7ff3cccae901225695e7fb4faf7b5ef4f3e523f50'
