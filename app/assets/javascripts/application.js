e8c4995bc44f7024318766ab545be4d4ba57e745
Fernando Martinez <femartinez@uc.cl>
2014-06-23 01:44:27 -0400

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,8 +26,8 @@ $(document).ready(function() {
 	var item_width = $('#slides li').width(); 
 	var left_value = item_width * (-1);         
     //move the last item before first item, just in case user click prev button
-	$('#slides li:first').before($('#slides li:last'));
-	
+    $('#slides li:first').before($('#slides li:last'));
+
 	//set the default item to the correct position 
 	$('#slides ul').css({'left' : left_value});
 	//if user clicked on prev button
@@ -40,69 +40,77 @@ $(document).ready(function() {
 		$('#slides ul').animate({'left' : left_indent}, 1200,function(){    
 
             //move the last item and put it as first item            	
-			$('#slides li:first').before($('#slides li:last'));           
+            $('#slides li:first').before($('#slides li:last'));           
 
 			//set the default item to correct position
 			$('#slides ul').css({'left' : left_value});
-		
+
 		});
 
 		//cancel the link behavior            
 		return false;
-            
+
 	});
 
- 
+
     //if user clicked on next button
-	$('#next').click(function() {
-		
+    $('#next').click(function() {
+
 		//get the right position
 		var left_indent = parseInt($('#slides ul').css('left')) - item_width;
 		
 		//slide the item
 		$('#slides ul').animate({'left' : left_indent}, 1200, function () {
-            
+
             //move the first item and put it as last item
-			$('#slides li:last').after($('#slides li:first'));                 	
-			
+            $('#slides li:last').after($('#slides li:first'));                 	
+
 			//set the default item to correct position
 			$('#slides ul').css({'left' : left_value});
-		
+
 		});
-		         
+
 		//cancel the link behavior
 		return false;
 		
 	});        
-	
+
 
 //if mouse hover, pause the auto rotation, otherwise rotate it
-	$('#slides').hover(
-		
-		function() {
-			clearInterval(run);
-		}, 
-		function() {
-			run = setInterval('rotate()', speed);	
-		}
+$('#slides').hover(
+
+	function() {
+		clearInterval(run);
+	}, 
+	function() {
+		run = setInterval('rotate()', speed);	
+	}
 	); 
 
-	$('.contacto_minimizado').hover(
-		function(){
-		$('.contacto_maximizado').css('display', 'block');
-		},function(){
-		$('.contacto_maximizado').css('display', 'none');
-		}
+$('.contacto_minimizado').click(
+	function(){
+		$('.contacto_maximizado').css('display', 'block').animate({"height": "70px"}, 500);
+		$(this).css('cursor', 'default');
+		//$('.contacto_maximizado').animate({"height": "70px"}, 500);
+	}//,function(){
+	//	if($('.contacto_maximizado').height()!='0px'){
+	//		$('.contacto_maximizado').css('height', '0px');
+	//	}
+	// }
 	);
-	$('.contacto_maximizado').hover(
-		function(){
-		$('.contacto_maximizado').css('display', 'block');
-		},function(){
-		$('.contacto_maximizado').css('display', 'none');
-		}
+$('.contacto_maximizado').hover(
+	function(){
+		//$(this).css('display', 'block');
+		//$(this).animate({"height": "70px"}, 500);
+	},function(){
+		$(this).animate({"height": "0px"}, 400,function(){
+			$(this).css('display', 'none');
+		});
+		$('.contacto_minimizado').css('cursor', 'pointer');
+	}
 	);
 
-        
+
 });
 
 function rotate() {
