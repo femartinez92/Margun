8523920bd986145757fe74cb33fe5ad8bef23132
Fernando Martinez <femartinez@uc.cl>
2014-06-23 17:38:24 -0400

    carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,75 +17,21 @@
 
 //Slider animation -------------------------
 $(document).ready(function() {
-
-	//rotation speed and timer
-	var speed = 5000;
-	var run = setInterval('rotate()', speed);	
-	
-	//grab the width and calculate left value
-	var item_width = $('#slides li').width(); 
-	var left_value = item_width * (-1);         
-    //move the last item before first item, just in case user click prev button
-    $('#slides li:first').before($('#slides li:last'));
-
-	//set the default item to the correct position 
-	$('#slides ul').css({'left' : left_value});
-	//if user clicked on prev button
-	$('#prev').click(function() {
-
-		//get the right position            
-		var left_indent = parseInt($('#slides ul').css('left')) + item_width;
-
-		//slide the item            
-		$('#slides ul').animate({'left' : left_indent}, 1200,function(){    
-
-            //move the last item and put it as first item            	
-            $('#slides li:first').before($('#slides li:last'));           
-
-			//set the default item to correct position
-			$('#slides ul').css({'left' : left_value});
-
-		});
-
-		//cancel the link behavior            
-		return false;
-
+	$('#carousel').carouFredSel({
+		responsive: true,
+		items: {
+			visible: 1,
+			width: 1280,
+			height: 243
+		},
+		scroll: {
+			duration: 1200,
+			timeoutDuration: 5000//,
+			//fx: 'uncover-fade'
+		},
+		pagination: '#pager'
 	});
 
-
-    //if user clicked on next button
-    $('#next').click(function() {
-
-		//get the right position
-		var left_indent = parseInt($('#slides ul').css('left')) - item_width;
-		
-		//slide the item
-		$('#slides ul').animate({'left' : left_indent}, 1200, function () {
-
-            //move the first item and put it as last item
-            $('#slides li:last').after($('#slides li:first'));                 	
-
-			//set the default item to correct position
-			$('#slides ul').css({'left' : left_value});
-
-		});
-
-		//cancel the link behavior
-		return false;
-		
-	});        
-
-
-//if mouse hover, pause the auto rotation, otherwise rotate it
-$('#slides').hover(
-
-	function() {
-		clearInterval(run);
-	}, 
-	function() {
-		run = setInterval('rotate()', speed);	
-	}
-	); 
 
 $('.contacto_minimizado').click(
 	function(){
